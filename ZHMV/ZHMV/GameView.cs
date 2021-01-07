d090c334e2baa9c7bb8ddbaf9230f38fe0d1437c
Borsy Máté <52719798+borsym@users.noreply.github.com>
2021-01-07 15:49:05 +0100

    Add files via upload
    undorító a kod de legalább működik xd

---
+++ ZHMV/ZHMV/GameView.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZHMV.Model;
+
+namespace ZHMV
+{
+    public partial class GameView : Form
+    {
+        private ModelMV _model; 
+        private Button[,] _buttonGrid; 
+        private Timer _timer;
+        private bool isStoped;
+        public GameView()
+        {
+            InitializeComponent();
+        }
+        private void GameView_Load(object sender, EventArgs e)
+        {
+            // modell létrehozása és az eseménykezelők társítása
+            _model = new ModelMV();
+            _model.GameAdvanced += new EventHandler<ModelEventArgs>(Game_GameAdvanced);
+            _model.GameOver += new EventHandler<ModelEventArgs>(Game_GameOver);
+
+            // időzítő létrehozása
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += new EventHandler(Timer_Tick);
+
+            // játéktábla és menük inicializálása
+            _model.NewGame(12);
+            GenerateTable();
+            SetupTable();
+            refreshTable();
+            MessageBox.Show("Kérlek indíts egy játékot!");
+           // _timer.Start();
+        }
+
+        private void Game_GameAdvanced(Object sender, ModelEventArgs e)
+        {
+                _LabelTime.Text = TimeSpan.FromSeconds(e.GameTime).ToString("g");
+                _LabelStep.Text = e.GameStepCount.ToString();
+            
+        }
+
+        private void Game_GameOver(Object sender, ModelEventArgs e)
+        {
+            _timer.Stop();
+
+            foreach (Button button in _buttonGrid) // kikapcsoljuk a gombokat
+                button.Enabled = false;
+
+
+            MessageBox.Show("Vege a játéknak!" + Environment.NewLine +
+                            "Összesen " + e.GameStepCount + " lépést tettél meg és " +
+                            TimeSpan.FromSeconds(e.GameTime).ToString("g") + " ideig játszottál.",
+                            "Game",
+                            MessageBoxButtons.OK);
+           
+        }
+
+     
+
+        private void DeleteTable()
+        {
+            for (Int32 i = 0; i < _model.Table.Size; i++) 
+            { 
+                for (Int32 j = 0; j < _model.Table.Size; j++)
+                {
+                    Controls.Remove(_buttonGrid[i, j]);
+                }
+            }
+        }
+
+        private void Pause_Game(object sender, EventArgs e)
+        {
+            if(isStoped)
+            {
+                _timer.Start();
+                isStoped = false;
+            }
+            else
+            {
+                isStoped = true;
+                _timer.Stop();
+            }
+        
+        }
+        private void MenuFileNewGame_Click(Object sender, EventArgs e)
+        {
+            isStoped = false;
+            DeleteTable();
+            _model.NewGame(_model.CurrentSize);
+            GenerateTable();
+            SetupTable();
+
+            _timer.Start();
+        }
+
+
+        private void MenuFileNewGame6_Click(Object sender, EventArgs e)
+        {
+            DeleteTable();
+            _model.NewGame(6);
+            GenerateTable();
+            SetupTable();
+
+            _timer.Start();
+        }
+
+        private void MenuFileNewGame8_Click(Object sender, EventArgs e)
+        {
+            DeleteTable();
+            _model.NewGame(8);
+            GenerateTable();
+            SetupTable();
+
+            _timer.Start();
+        }
+
+        private void MenuFileNewGame10_Click(Object sender, EventArgs e)
+        {
+            DeleteTable();
+            _model.NewGame(10);
+            GenerateTable();
+            SetupTable();
+
+            _timer.Start();
+        }
+
+        private void Timer_Tick(Object sender, EventArgs e)
+        {
+            _model.AdvanceTime(); // játék léptetése
+            refreshTable();
+        }
+
+
+        private void GenerateTable()
+        {
+            _buttonGrid = new Button[_model.Table.Size, _model.Table.Size];
+            for (Int32 i = 0; i < _model.Table.Size; i++)
+                for (Int32 j = 0; j < _model.Table.Size; j++)
+                {
+                    _buttonGrid[i, j] = new Button();
+                    _buttonGrid[i, j].Location = new Point(5 + 50 * j, 35 + 50 * i); // elhelyezkedés
+                    _buttonGrid[i, j].Size = new Size(50, 50); // méret
+                    _buttonGrid[i, j].Enabled = false; // kikapcsolt állapot
+                    _buttonGrid[i, j].TabIndex = 100 + i * _model.Table.Size + j; // a gomb számát a TabIndex-ben tároljuk
+                    _buttonGrid[i, j].FlatStyle = FlatStyle.Flat; // lapított stípus
+                   // _buttonGrid[i, j].MouseClick += new MouseEventHandler(ButtonGrid_MouseClick);
+                    // közös eseménykezelő hozzárendelése minden gombhoz
+
+                    Controls.Add(_buttonGrid[i, j]);
+                    // felvesszük az ablakra a gombot
+                }
+        }
+
+        private void refreshTable()
+        {
+            for (Int32 i = 0; i < _model.Table.Size; i++)
+            {
+                for (Int32 j = 0; j < _model.Table.Size; j++)
+                {
+                    _buttonGrid[i, j].Text = _model.Table.IsEmpty(i, j)
+                       ? String.Empty
+                       : _model.Table[i, j].ToString();
+                    switch (_model.Table.GetValue(i, j))
+                    {
+                        case 0:
+                            _buttonGrid[i, j].BackColor = Color.White;
+                            break;
+                        case 1:
+                            _buttonGrid[i, j].BackColor = Color.Yellow;
+                            break;
+                        case 2:
+                            _buttonGrid[i, j].BackColor = Color.Red;
+                            break;
+                        case 3:
+                            _buttonGrid[i, j].BackColor = Color.Black;
+                            break;
+                        case 4:
+                            _buttonGrid[i, j].BackColor = Color.Blue;
+                            break;
+                    }
+                }
+            }
+
+        }
+        private void SetupTable()
+        {
+            for (Int32 i = 0; i < _buttonGrid.GetLength(0); i++)
+            {
+                for (Int32 j = 0; j < _buttonGrid.GetLength(1); j++)
+                {
+                    _buttonGrid[i, j].Text = _model.Table.IsEmpty(i, j)
+                        ? String.Empty
+                        : _model.Table[i, j].ToString();
+                    _buttonGrid[i, j].Enabled = true;
+                    _buttonGrid[i, j].BackColor = Color.White;
+
+                }
+            }
+
+            _LabelTime.Text = TimeSpan.FromSeconds(_model.GameTime).ToString("g");
+            _LabelStep.Text = _model.PointCount.ToString();
+            refreshTable();
+        }
+
+        private void GameView_KeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.Write(e.KeyCode + "\n");
+
+            if(e.KeyCode == Keys.A)
+            {
+                _model.movePackMan(0,-1);
+            }
+            else if(e.KeyCode == Keys.D)
+            {
+                _model.movePackMan(0, 1);
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                _model.movePackMan(1, 0);
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                _model.movePackMan(-1, 0);
+            }
+
+            refreshTable();
+        }
+    }
+}
---
+++ ZHMV/ZHMV/Model/ModelMV.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZHMV.Model
+{
+    class ModelMV
+    {
+        private ModelTable _table;
+        private Int32 _pointCount;
+        private Int32 _gameTime;
+        private Int32 _currentSize;
+        public KeyValuePair<int, int> packMan;
+        public List<KeyValuePair<int, int>> ghostsContainer;
+        public List<KeyValuePair<int, int>> walls;
+        public List<KeyValuePair<int, int>> bogyok;
+        public Int32 PointCount { get { return _pointCount; } }
+        public Int32 GameTime { get { return _gameTime; } }
+        public ModelTable Table { get { return _table; } }
+        public Boolean IsGameOver { get { return false; } }
+        public int CurrentSize { get => _currentSize;}
+
+        public event EventHandler<ModelEventArgs> GameAdvanced;
+        public event EventHandler<ModelEventArgs> GameOver;
+
+        public ModelMV()
+        {
+            _currentSize = 12;
+            _table = new ModelTable();
+        }
+
+        public void NewGame(int size)
+        {
+            packMan = new KeyValuePair<int, int>(_table.Size - 1, 0);
+            walls =  new List<KeyValuePair<int, int>>();
+            bogyok = new List<KeyValuePair<int, int>>();
+            ghostsContainer = new List<KeyValuePair<int, int>>();
+        _table = new ModelTable(size);
+            _currentSize = size;
+            _pointCount = 0;
+            _gameTime = 0;
+            GenerateFields();
+        }
+
+        private void CheckGameOver()
+        {
+            for(int i = 0; i < _table.Size; i++)
+            {
+                for(int j = 0; j < _table.Size; j++)
+                {
+                    if (_table.GetValue(i, j) == 4) return;
+                }
+            }
+            OnGameOver(true);
+        }
+        private void MoveRandomGhost()
+        {
+            Random rand = new Random();
+            int randomN = rand.Next(0, 8);
+           // int count = 0;
+            for(int i = 0; i < ghostsContainer.Count /*|| count < _table.Size / 3*/; i++)
+            {
+                switch(randomN)
+                {
+                    //fel le jobb bal
+                    case 0:
+                        if(ghostsContainer[i].Key + 1 < _table.Size && _table.GetValue(ghostsContainer[i].Key + 1, ghostsContainer[i].Value) != 2 && _table.GetValue(ghostsContainer[i].Key + 1, ghostsContainer[i].Value) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key + 1, ghostsContainer[i].Value);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                           // count++;
+                        }
+                        break;
+                    case 1:
+                        if (ghostsContainer[i].Key - 1 >= 0 && _table.GetValue(ghostsContainer[i].Key - 1, ghostsContainer[i].Value) != 2 && _table.GetValue(ghostsContainer[i].Key - 1, ghostsContainer[i].Value) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key - 1, ghostsContainer[i].Value);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++; 
+                        }
+                        break;
+                    case 2:
+                        if (ghostsContainer[i].Value - 1 >= 0 && _table.GetValue(ghostsContainer[i].Key, ghostsContainer[i].Value - 1) != 2 && _table.GetValue(ghostsContainer[i].Key , ghostsContainer[i].Value - 1) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key  ,ghostsContainer[i].Value - 1);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++;
+                        }
+                        break;
+                    case 3:
+                        if (ghostsContainer[i].Value + 1 <_table.Size && _table.GetValue(ghostsContainer[i].Key, ghostsContainer[i].Value + 1) != 2 && _table.GetValue(ghostsContainer[i].Key, ghostsContainer[i].Value + 1) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key, ghostsContainer[i].Value + 1);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++;
+                        }
+                        break;
+                    case 4:
+                        if (ghostsContainer[i].Value + 1 < _table.Size && ghostsContainer[i].Key + 1 < _table.Size && _table.GetValue(ghostsContainer[i].Key + 1, ghostsContainer[i].Value + 1) != 2 && _table.GetValue(ghostsContainer[i].Key + 1, ghostsContainer[i].Value + 1) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key + 1, ghostsContainer[i].Value + 1);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++;
+                        }
+                        break;
+                    case 5:
+                        if (ghostsContainer[i].Value - 1 >= 0 && ghostsContainer[i].Key + 1 < _table.Size && _table.GetValue(ghostsContainer[i].Key + 1, ghostsContainer[i].Value - 1) != 2 && _table.GetValue(ghostsContainer[i].Key + 1, ghostsContainer[i].Value - 1) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key + 1, ghostsContainer[i].Value - 1);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++;
+                        }
+                        break;
+                    case 6:
+                        if (ghostsContainer[i].Value + 1 < _table.Size && ghostsContainer[i].Key - 1 >= 0 && _table.GetValue(ghostsContainer[i].Key - 1, ghostsContainer[i].Value + 1) != 2 && _table.GetValue(ghostsContainer[i].Key - 1, ghostsContainer[i].Value + 1) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key - 1, ghostsContainer[i].Value + 1);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++;
+                        }
+                        break;
+                    case 7:
+                        if (ghostsContainer[i].Value - 1 >= 0 && ghostsContainer[i].Key - 1 >= 0 && _table.GetValue(ghostsContainer[i].Key - 1, ghostsContainer[i].Value - 1) != 2 && _table.GetValue(ghostsContainer[i].Key - 1, ghostsContainer[i].Value - 1) != 3)
+                        {
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 0);
+                            ghostsContainer[i] = new KeyValuePair<int, int>(ghostsContainer[i].Key - 1, ghostsContainer[i].Value - 1);
+                            _table.SetValue(ghostsContainer[i].Key, ghostsContainer[i].Value, 2);
+                            //count++;
+                        }
+                        break;
+
+                }
+                KeyValuePair<int, int> tmp = new KeyValuePair<int, int>(packMan.Key, packMan.Value);
+                if (ghostsContainer.Contains(tmp))
+                {
+                    OnGameOver(true);
+                }
+            }
+        }
+        private void putBackBogyok()
+        {
+            for(int i = 0; i < bogyok.Count; i++)
+            {
+                if(_table.GetValue(bogyok[i].Key, bogyok[i].Value) != 2) _table.SetValue(bogyok[i].Key, bogyok[i].Value, 4);
+            }
+        }
+
+        public void AdvanceTime()
+        {
+            if (IsGameOver)
+                return;
+
+            _gameTime++;
+            MoveRandomGhost();
+            putBackBogyok();
+            CheckGameOver();
+            OnGameAdvanced();
+
+            //if (_gameTime == 99) 
+            //    OnGameOver(false);
+        }
+
+       
+
+        private void putPackMan()
+        {
+            _table.SetValue(_table.Size - 1, 0, 1);
+        }
+
+        private void ghosts()
+        {
+            Random rand = new Random();
+            int j = rand.Next(0, _table.Size);
+            for(int i = 0; i < _table.Size / 3; i++)
+            {
+                while(_table.GetValue(0,j) != 0)
+                {
+                    j = rand.Next(0, _table.Size);
+                }
+                _table.SetValue(0, j, 2);
+                ghostsContainer.Add(new KeyValuePair<int, int>(0, j));
+            }
+        }
+
+        // 3 fal 4 bogyo
+        private void putWallsAndBogyok()
+        {
+            
+            for(int i = 1; i <_table.Size - 1; i++)
+            {
+                _table.SetValue(i, 2, 3);
+                walls.Add(new KeyValuePair<int, int>(i, 2));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 4, 3);
+                walls.Add(new KeyValuePair<int, int>(i, 4));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 6, 3);
+                walls.Add(new KeyValuePair<int, int>(i, 6));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 8, 3);
+                walls.Add(new KeyValuePair<int, int>(i, 8));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 10, 3);
+                walls.Add(new KeyValuePair<int, int>(i, 10));
+            }
+
+
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 1, 4);
+                bogyok.Add(new KeyValuePair<int, int>(i, 1));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 3, 4);
+                bogyok.Add(new KeyValuePair<int, int>(i, 3));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 5, 4);
+                bogyok.Add(new KeyValuePair<int, int>(i, 5));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 7, 4);
+                bogyok.Add(new KeyValuePair<int, int>(i, 7));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 9, 4);
+                bogyok.Add(new KeyValuePair<int, int>(i, 9));
+            }
+
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, 0, 4);
+                bogyok.Add(new KeyValuePair<int, int>(i, 0));
+            }
+            for (int i = 1; i < _table.Size - 1; i++)
+            {
+                _table.SetValue(i, _table.Size - 1, 3);
+                walls.Add(new KeyValuePair<int, int>(i, _table.Size - 1));
+            }
+
+
+            for(int i = 1; i < _table.Size; i++)
+            {
+                _table.SetValue(_table.Size - 1, i, 4);
+                bogyok.Add(new KeyValuePair<int, int>(_table.Size - 1, i));
+
+            }
+
+            for(int i = 0; i < _table.Size; i++)
+            {
+                if (_table.GetValue(0, i) != 2)
+                {
+                    _table.SetValue(0, i, 4);
+                    bogyok.Add(new KeyValuePair<int, int>(0, i));
+                }
+            }
+
+        }
+
+        private void GenerateFields()
+        {
+            for(Int32 i = 0; i < _table.Size; i++)
+            {
+                for(Int32 j = 0; j < _table.Size; j++)
+                {
+                    _table.SetValue(i, j, 0);
+                }
+            }
+            ghosts();
+            putPackMan();
+            putWallsAndBogyok();
+        }
+
+        private void OnGameAdvanced()
+        {
+            if (GameAdvanced != null)
+                GameAdvanced(this, new ModelEventArgs(false, _pointCount, _gameTime));
+        }
+        private void OnGameOver(Boolean isWon)
+        {
+            if (GameOver != null)
+                GameOver(this, new ModelEventArgs(isWon, _pointCount, _gameTime));
+        }
+
+        public void movePackMan(int x, int y)
+        {
+            if (packMan.Key + x < 0 || packMan.Key + x >= _table.Size || packMan.Value + y >= _table.Size || packMan.Value + y < 0) return;
+            if (_table.GetValue(packMan.Key + x, packMan.Value + y) == 3) return;
+
+
+            _table.SetValue(packMan.Key, packMan.Value, 0);
+            KeyValuePair<int, int> tmp = new KeyValuePair<int, int>(packMan.Key + x, packMan.Value + y);
+            if (_table.GetValue(packMan.Key + x, packMan.Value + y) == 4)
+            {
+                
+                bogyok.Remove(tmp);
+                _pointCount++;
+                OnGameAdvanced();
+            }
+            if(ghostsContainer.Contains(tmp))
+            {
+                OnGameOver(true);
+            }
+            packMan = new KeyValuePair<int, int>(packMan.Key + x, packMan.Value + y);
+            _table.SetValue(packMan.Key, packMan.Value, 1);
+        }
+    }
+}
